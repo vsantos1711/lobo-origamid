6293315e2e1ba8c394b83a8bd2fe0558748e8537
Vinícius Santos <vsantos067100@gmail.com>
2022-07-19 09:47:28 -0300

    Finalização do projeto e menu hamburguer

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-btnMobile.addEventListener('click', toggleMenu)
 
 function toggleMenu(){
     const menu = document.getElementById('menu');
     menu.classList.toggle('active')
 }
+
+
+btnMobile.addEventListener('click', toggleMenu);
 
 
